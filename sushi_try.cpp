18996a0212a2ea9c94f930bf3aa36ce9e5caff9a
k29 <kaustubh.nawade@gmail.com>
2014-03-24 04:28:43 +0530

    added itemmateadata function

--- sushi_try.cpp
+++ sushi_try.cpp
@@ -25,6 +25,21 @@ void extract_user_metadata(SGMatrix<int32_t>& user_metadata)
 	cout<<"num_rows "<<user_metadata.num_rows;
 }
 
+void extract_item_metadata(SGMatrix<float32_t>& item_metadata)
+{
+	CCSVFile *fin;
+	fin=new CCSVFile("sushi3.idata",'r',NULL);
+	fin->set_delimiter('	');
+	fin->get_matrix(item_metadata.matrix,item_metadata.num_cols,item_metadata.num_rows);
+
+	int32_t rows=1;
+	int32_t cols=11;
+	item_metadata.display_matrix(item_metadata.matrix,rows,cols,"item_metadata","");
+	
+	cout<<"num_cols "<<item_metadata.num_cols<<"\n";
+	cout<<"num_rows "<<item_metadata.num_rows;
+}
+
 int main(int argc, char const *argv[])
 {
 	init_shogun(&print_message);
@@ -32,8 +47,8 @@ int main(int argc, char const *argv[])
 	SGMatrix<int32_t> user_metadata(true);
 	extract_user_metadata(user_metadata);
 
-	
-	
+	SGMatrix<float32_t> item_metadata(true);
+	extract_item_metadata(item_metadata);
 
 	return 0;
 }
