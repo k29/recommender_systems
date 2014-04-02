0be01c293a6c9661bbdd392f00fa7889f6aea6f1
k29 <kaustubh.nawade@gmail.com>
2014-04-02 10:20:30 +0530

    started parsin items data

--- movie_lens/movielens.cpp
+++ movie_lens/movielens.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <stdio.h>
+#include <shogun/io/File.h>
 #include <shogun/io/CSVFile.h>
+#include <shogun/io/LineReader.h>
 #include <shogun/lib/SGMatrix.h>
 #include <eigen3/Eigen/Sparse>
+#include <shogun/lib/DelimiterTokenizer.h>
 
 using namespace shogun;
 using namespace std;
@@ -12,7 +16,7 @@ void print_message(FILE* target, const char* str)
 }
 
 
-void user_rating(SGMatrix<int32_t> &userRatings) //user_ratings has the data in the 943*1682 matrix
+void extract_user_rating(SGMatrix<int32_t> &userRatings) //user_ratings has the data in the 943*1682 matrix
 {
 	SGMatrix<int32_t> input_udata(true); //matrix to take in the raw input
 
@@ -35,6 +39,28 @@ void user_rating(SGMatrix<int32_t> &userRatings) //user_ratings has the data in 
 		userRatings(user_id-1,movie_id-1)=rating;
 	}
 	//userRatings.display_matrix("userRatings");
+}
+
+
+void extract_movie_metadata(SGMatrix<int32_t> &movieMetadata)
+{
+	SGMatrix<int32_t> input_mdata(true); //matrix to take in the raw input
+
+	FILE *fin;
+	fin=new FILE("ml-100k/u.item",'r',NULL);
+
+	SGVector<char> line;
+
+	CDelimiterTokenizer *tokenLine=new CDelimiterTokenizer(true);
+	tokenLine->delimiters['\n']=1;
+	SG_REF(tokenLine);
+
+	CDelimiterTokenizer *tokenizer=new CDelimiterTokenizer(true);
+	tokenizer->delimiters['|']=1;
+	SG_REF(tokenizer);
+	
+	CLineReader *reader=new CLineReader(fin,tokenLine);
+
 
 }
 
@@ -42,11 +68,11 @@ int main(int argc, char const *argv[])
 {
 	init_shogun(&print_message);
 
-	index_t nrows_users=943;
-	index_t ncols_users=1682;	
-	SGMatrix<int32_t> userRatings(nrows_users,ncols_users,true); //defining the user rating matrix having the correspoinding rating in the 943x1682 matrix
+	SGMatrix<int32_t> userRatings(943,1682,true); //defining the user rating matrix having the correspoinding rating in the 943x1682 matrix
+	extract_user_rating(userRatings);
 
-	user_rating(userRatings);
+	SGMatrix<int32_t> movieMetadata(1682,19,true); //contains genre info
+	extract_movie_metadata(movieMetadata);
 	
 
 	return 0;
